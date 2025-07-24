689a75a8c69bb005df31f4ccea0b53cad291bbfd
Sahana <sahanabalajikumar@gmail.com>
2025-07-24 12:59:01 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Sahana on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
